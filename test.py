41fb869c1f5688739761d4eecb11a7159a0559fa
Michael Huang <sleep3030@gmail.com>
2019-11-13 00:10:46 -0800

    Added full text support

--- test.py
+++ test.py
@@ -1,16 +1,20 @@
-import selenium as sm
+
 from selenium import webdriver 
+#from nltk import WordNetLemmatizer
 import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import wordnet
 import time
+import os
 import speech_recognition as sr
-import urllib.request
 import requests
 
-def download_file(url):
-    local_filename = url.split('/')[-1]
+def download_file(url, word):
+    local_filename =  "C:/Users/Michael Huang/Documents/GitHub/TexttoASL/Signs/" + word + ".mp4"
     # NOTE the stream=True parameter
     r = requests.get(url, stream=True)
     with open(local_filename, 'wb') as f:
+        #print(local_filename)
         for chunk in r.iter_content(chunk_size=1024): 
             if chunk: # filter out keep-alive new chunks
                 f.write(chunk)
@@ -24,12 +28,10 @@ def videos(word):
     site = "https://www.signasl.org/sign/" + str(word)
     browser.get(site)
     vid = browser.find_element_by_class_name("vjs-tech").get_attribute("src")
-    #print(vid)
-    #urllib.request.urlretrieve(vid, "test.mp4")
-    #browser.quit()
-    #print(r)
+    browser.quit()
+
     #return vid
-    download_file(vid)
+    download_file(vid, word)
 
 
 
@@ -46,6 +48,21 @@ def Speech(recognizer, microphone):
         response = "Bad Speech"
     return response
 
+
+def get_wordnet_pos(word):
+    """Map POS tag to first character lemmatize() accepts"""
+    tag = nltk.pos_tag([word])[0][1][0].upper()
+  #  print(nltk.pos_tag([word])[0])
+    tag_dict = {"J": wordnet.ADJ,
+                "N": wordnet.NOUN,
+                "V": wordnet.VERB,
+                "R": wordnet.ADV}
+   # print(tag_dict.get(tag, wordnet.NOUN))
+    return tag_dict.get(tag, wordnet.NOUN)
+
+
+
+
 if __name__ == "__main__":
     #recognizer = sr.Recognizer()
     #microphone = sr.Microphone(device_index=0)
@@ -54,6 +71,9 @@ if __name__ == "__main__":
     #print("Hello! This is a basic speech understander")
     #Words = Speech(recognizer, microphone)
     #print("Said:", guess)
-    me = videos("how")
+    
+    lemma = WordNetLemmatizer()
+    word = (lemma.lemmatize("walking", get_wordnet_pos("walking")))
+    me = videos(word)
     #while(True):
     #    time.sleep(2)

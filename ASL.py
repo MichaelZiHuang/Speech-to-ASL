fadf81175b72abf119677656c0f526547bace9be
Michael Huang <sleep3030@gmail.com>
2019-11-16 17:51:23 -0800

    Switching website and databases

--- ASL.py
+++ ASL.py
@@ -1,16 +1,17 @@
 
 from selenium import webdriver 
-#from nltk import WordNetLemmatizer
+from selenium.common.exceptions import NoSuchElementException
 import nltk
+from nltk.tokenize import RegexpTokenizer
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import wordnet
-import time
-import os
 import speech_recognition as sr
 import requests
+from os import listdir
+OS_PATH = "C:/Users/Michael Huang/Documents/GitHub/TexttoASL/Signs/"
 
 def download_file(url, word):
-    local_filename =  "C:/Users/Michael Huang/Documents/GitHub/TexttoASL/Signs/" + word + ".mp4"
+    local_filename =  OS_PATH + word + ".mp4"
     # NOTE the stream=True parameter
     r = requests.get(url, stream=True)
     with open(local_filename, 'wb') as f:
@@ -24,14 +25,19 @@ def download_file(url, word):
 
 
 def videos(word):
-    browser = webdriver.Chrome()
-    site = "https://www.signasl.org/sign/" + str(word)
-    browser.get(site)
-    vid = browser.find_element_by_class_name("vjs-tech").get_attribute("src")
-    browser.quit()
+    driver = webdriver.Chrome()
+    site = "https://www.signingsavvy.com/search/" + str(word)
+    driver.get(site)
 
-    #return vid
-    download_file(vid, word)
+    try:
+        driver.find_element_by_link_text(word.upper()).click()
+    except: 
+        pass
+    vid = driver.find_element_by_class_name("vjs-tech").get_attribute("src")
+  
+    driver.quit()
+
+    return download_file(vid, word)
 
 
 
@@ -61,19 +67,52 @@ def get_wordnet_pos(word):
     return tag_dict.get(tag, wordnet.NOUN)
 
 
+def process_words(phrase):
+    #ASL does not use any articles in its speech.
+    bad = {"am", "is", "are", "was", "were", "a", "an", "are"}
+    tokenizer = RegexpTokenizer(r'\w+')
+    word = tokenizer.tokenize(phrase)
+    lemma = WordNetLemmatizer()
+    
+    for w in word:
+        if w not in bad:
+            yield lemma.lemmatize(w, get_wordnet_pos(w))
+        else if w == "my" or w == "i":
+            yield "me"
+
+def check_db(): # Checks if a video for a word has already been downloaded. 
+    vids = listdir(OS_PATH)
+    already_have = {}
+    for v in vids:
+        already_have[v[:-4]] = None
+    return already_have
+
+def collect_vids(db, phrase):
+    vidtxt = open("vids", "w")
+    missing = []
+    for w in phrase:
+        if w in db:
+            # grab word
+            vidtxt.write(OS_PATH + w + ".mp4")
+        else:  
+            vidtxt.write(videos(w))
+            # download video
 
 
 if __name__ == "__main__":
     #recognizer = sr.Recognizer()
     #microphone = sr.Microphone(device_index=0)
     #names = microphone.list_microphone_names()
-    #print(names)
-    #print("Hello! This is a basic speech understander")
+    print(
+        "Hello! This is a basic speech understander. \nIt's capable of both speech and text to ASL capabilities (in PSE)"
+        )
     #Words = Speech(recognizer, microphone)
-    #print("Said:", guess)
+    phrase = "This is my word testing?"
+    tokened = lambda word: list(process_words(word))
+    translated = tokened(phrase)
+    wordlist = check_db()
+    vids = collect_vids(wordlist, translated)
+
     
-    lemma = WordNetLemmatizer()
-    word = (lemma.lemmatize("walking", get_wordnet_pos("walking")))
-    me = videos(word)
-    #while(True):
-    #    time.sleep(2)
+    #me = videos("me")
+

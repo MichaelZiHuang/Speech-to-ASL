3fc32ed7362b3e3bb49a0f5ba43aeec66959dce0
Michael Huang <sleep3030@gmail.com>
2019-11-18 00:01:14 -0800

    Finished Prohect

--- ASL.py
+++ ASL.py
@@ -7,7 +7,11 @@ from nltk.stem import WordNetLemmatizer
 from nltk.corpus import wordnet
 import speech_recognition as sr
 import requests
-from os import listdir
+import os
+import subprocess
+
+
+
 OS_PATH = "C:/Users/Michael Huang/Documents/GitHub/TexttoASL/Signs/"
 
 def download_file(url, word):
@@ -24,34 +28,37 @@ def download_file(url, word):
 
 
 
-def videos(word):
+def videos(phrase):
     driver = webdriver.Chrome()
-    site = "https://www.signingsavvy.com/search/" + str(word)
-    driver.get(site)
+    site = "https://www.signingsavvy.com/search/"
 
-    try:
-        driver.find_element_by_link_text(word.upper()).click()
-    except: 
-        pass
-    vid = driver.find_element_by_class_name("vjs-tech").get_attribute("src")
+    for w in phrase:
+        driver.get(site + str(w))
+        try:
+            driver.find_element_by_link_text(w.upper()).click()
+        except: 
+            pass
+        vid = driver.find_element_by_class_name("vjs-tech").get_attribute("src")
+        download_file(vid, w)
   
     driver.quit()
-
-    return download_file(vid, word)
+    return None
+    #return download_file(vid, word)
 
 
 
 
 def Speech(recognizer, microphone):
+    print("Please say your phrase")
     with microphone as source:
         recognizer.adjust_for_ambient_noise(source)
         audio = recognizer.listen(source)
     try:
         response = recognizer.recognize_google(audio)
     except sr.RequestError:
-        response = "Fail"
+        response = "1"
     except sr.UnknownValueError:
-        response = "Bad Speech"
+        response = "2"
     return response
 
 
@@ -76,43 +83,67 @@ def process_words(phrase):
     
     for w in word:
         if w not in bad:
-            yield lemma.lemmatize(w, get_wordnet_pos(w))
-        else if w == "my" or w == "i":
-            yield "me"
+            yield lemma.lemmatize(w.lower(), get_wordnet_pos(w))
 
 def check_db(): # Checks if a video for a word has already been downloaded. 
-    vids = listdir(OS_PATH)
+    vids = os.listdir(OS_PATH)
     already_have = {}
     for v in vids:
         already_have[v[:-4]] = None
     return already_have
 
 def collect_vids(db, phrase):
-    vidtxt = open("vids", "w")
+    vidtxt = open('vids.txt', 'w')
     missing = []
     for w in phrase:
-        if w in db:
-            # grab word
-            vidtxt.write(OS_PATH + w + ".mp4")
-        else:  
-            vidtxt.write(videos(w))
-            # download video
 
+        if w not in db:
+            missing.append(w)
+        vidtxt.write("file 'Signs/" + w + ".mp4'\n")
+    if missing != []:
+       videos(missing)
+    vidtxt.close()
+ 
+def runTranslate(translated):
+    wordlist = check_db()
+    collect_vids(wordlist, translated)
+
+    try:
+        os.remove('C:/Users/Michael Huang/Documents/GitHub/TexttoASL/output.mp4')
+    except:
+        pass
+    command = ['ffmpeg', '-f', 'concat', '-safe', '0',  '-i', 'C:/Users/Michael Huang/Documents/GitHub/TexttoASL/vids.txt', '-c', 'copy', 'output.mp4']
+    subprocess.call(command, shell=True)
+
+
+def runSpeech():
+    recognizer = sr.Recognizer()
+    microphone = sr.Microphone(device_index=1)
+    Words = Speech(recognizer, microphone)
+    print("You said:", Words)
+    if(Words == "1" or Words == "2"):
+        print("Voice Capture Failed")
+        return None
+    translated = tokened(Words)
+    runTranslate(translated)
+
+def runText():
+    phrase = input("Input your phrase: ")
+    translated = tokened(str(phrase))
+    runTranslate(translated)
 
 if __name__ == "__main__":
-    #recognizer = sr.Recognizer()
-    #microphone = sr.Microphone(device_index=0)
-    #names = microphone.list_microphone_names()
     print(
         "Hello! This is a basic speech understander. \nIt's capable of both speech and text to ASL capabilities (in PSE)"
-        )
-    #Words = Speech(recognizer, microphone)
-    phrase = "This is my word testing?"
-    tokened = lambda word: list(process_words(word))
-    translated = tokened(phrase)
-    wordlist = check_db()
-    vids = collect_vids(wordlist, translated)
+    )
+    choice = input("For choices, do 1 for Speech, or 2 for Text: ")
 
-    
-    #me = videos("me")
+    tokened = lambda word: list(process_words(word))
+    if(int(choice) == 1):
+        runSpeech()
+    elif(int(choice) == 2):
+        runText()
+    else:
+        print("You put in the wrong stuff.")
+   
 
